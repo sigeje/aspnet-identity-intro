5f1a0c3d7ddac5a111ae06cd46879ae02a61dadc
Gilang Jalu Selo <Gilang_S@mitrais.com>
2016-12-06 15:38:02 +0700

    Init project

---
+++ aspnet-identity-intro/aspnet-identity-intro/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="aspnet_identity_intro.MvcApplication" Language="C#" %>
---
+++ aspnet-identity-intro/aspnet-identity-intro/Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+namespace aspnet_identity_intro.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<aspnet_identity_intro.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(aspnet_identity_intro.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
