fb6b0274e96664bc88419e6be7338f61a8512917
Gilang Jalu Selo <Gilang_S@mitrais.com>
2016-12-06 15:38:02 +0700

    Init project

---
+++ aspnet-identity-intro/aspnet-identity-intro/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(aspnet_identity_intro.Startup))]
+namespace aspnet_identity_intro
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
